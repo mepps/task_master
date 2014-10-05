4b085e413d3d65d66fd5fe1ff47b5092d386f582
mepps <maggie.epps@gmail.com>
2014-10-05 19:27:19 -0400

    Fixed bug where goal wouldn't save without max date

--- app/controllers/goals_controller.rb
+++ app/controllers/goals_controller.rb
@@ -1,7 +1,7 @@
 class GoalsController < ApplicationController
 	def index
 		if session[:logged_in]
-			@goals = User.find(session[:user_id]).goals.where("date >= '#{Date.today}'")
+			@goals = User.find(session[:user_id]).goals#.where("date >= '#{Date.today}'")
 		else 
 			redirect_to new_session_path
 		end
@@ -16,21 +16,47 @@ class GoalsController < ApplicationController
 
 	def create
 		@goal = User.find(session[:user_id]).goals.new(goal: params[:goal])
-		@goal.date = User.find(session[:user_id]).goals.maximum('date') + 1
-		@goal.save
-
-		respond_to do |format|
-			format.html{redirect_to goals_path}
-			format.js
+		if params[:date] == ""
+			if maxdate = User.find(session[:user_id]).goals.maximum('date') && maxdate != nil
+				@goal.date = maxdate + 1
+			else
+				@goal.date = Time.now.to_s
+			end
+		else
+			@goal.date = params[:date]
 		end
-		
+		if @goal.save
+			respond_to do |format|
+				format.html{redirect_to goals_path}
+				format.js
+			end
+		# end	
+		else 
+			redirect_back flash[:alert] == 'error'
+		end	
+	end
+
+	def no_goal
 	end
 
 	def get_goal
 		@goal = Goal.where("date = ? AND user_id = ?",  params[:date], session[:user_id]).first
-		@date = params[:date]
-		# redirect_to "/users/#{session[:user_id]}"
+		date = params[:date].split("-")
+		@date = "#{date[1]}/#{date[2]}/#{date[0]}"
+			respond_to do |format|
+				format.html{redirect_to "/users/#{session[:user_id]}"}
+				format.js
+			end
 
+	end
+
+	def destroy
+		@goal = Goal.find(params[:id])
+		@goal.destroy
+		respond_to do |format|
+			format.html {redirect_to "/goals/#{session[:current_goal_id]}"}
+			format.js
+		end
 	end
 
 end
